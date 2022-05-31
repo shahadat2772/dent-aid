276b5ad83793eba7992487e1c43dc997a327717f
shahadat2772 <shahadat2772@gmailcom>
2022-05-31 20:21:31 +0600

    slightly improved the footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -10,16 +10,46 @@ const Footer = () => {
     <div className="footerComponent">
       <footer className="footer">
         <div className="footerContainer">
-          <h3 className="text-white">Newsletter</h3>
+          <div className="links">
+            <h5 className="text-white">SERVICES</h5>
+            <a href="">Branding</a>
+            <a href="">Design</a>
+            <a href="">Marketing</a>
+            <a href="">Advertisement</a>
+          </div>
+          <div className="links">
+            <h5 className="text-white">LEGAL</h5>
+            <a href="">Terms of use</a>
+            <a href="">Privacy policy</a>
+            <a href="">Cookie policy</a>
+          </div>
+          <div className="links">
+            <h5 className="text-white">COMPANY</h5>
+            <a href="">About us</a>
+            <a href="">Contact</a>
+            <a href="">Jobs</a>
+            <a href="">Press kit</a>
+          </div>
+          <div className="newsTeller">
+            <h5 style={{ marginBottom: "5px" }} className="text-white">
+              Newsletter
+            </h5>
 
-          <p className="text-white">
-            Subscribe to our newsletter and get <br /> 10% discount for your
-            next appointment.
-          </p>
+            <p
+              style={{
+                fontSize: "14px",
+                marginBottom: "8px",
+              }}
+              className="text-white"
+            >
+              Subscribe to our newsletter and get <br /> 10% discount for your
+              next appointment.
+            </p>
 
-          <div className="upperFooterInputAndBtnContainer">
-            <input type="" placeholder="Enter your email..." />
-            <button>Subscribe</button>
+            <div className="upperFooterInputAndBtnContainer">
+              <input type="" placeholder="Enter your email..." />
+              <button>Subscribe</button>
+            </div>
           </div>
         </div>
         <p className="copyrightText">© {year} DentAid. All Rights Reserved</p>
