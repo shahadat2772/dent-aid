7f2cfde0537a79d1db4bd2e60fdef1954fb05d4c
shahadat2772 <shahadat2772@gmailcom>
2022-05-31 19:37:01 +0600

    removed fack data

--- src/Pages/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment.js
@@ -2,53 +2,7 @@ import React, { useEffect, useState } from "react";
 import Loading from "../Shared/Loading/Loading";
 import "./Appointment.css";
 
-// import services1 from "../../images/services/basic-checkup.png";
-// import services2 from "../../images/services/whitning.png";
-// import services3 from "../../images/services/implants.png";
-// import services4 from "../../images/services/orthodontics.png";
-// import services5 from "../../images/services/root-canals.png";
-// import services6 from "../../images/services/fillings.png";
-
 import { useParams } from "react-router-dom";
-
-const services = [
-  {
-    id: 1,
-    name: "Basic Checkup",
-    price: 11.99,
-    description: "Lorem it is short description",
-  },
-  {
-    id: 2,
-    name: "Oral Hygiene",
-    price: 13.99,
-    description: "Lorem it is short description",
-  },
-  {
-    id: 3,
-    name: "Implant",
-    price: 99.99,
-    description: "Lorem it is short description",
-  },
-  {
-    id: 4,
-    name: "Orthodontics",
-    price: 111.99,
-    description: "Lorem it is short description",
-  },
-  {
-    id: 5,
-    name: "Root Canal",
-    price: 49.99,
-    description: "Lorem it is short description",
-  },
-  {
-    id: 6,
-    name: "Filling",
-    price: 29.99,
-    description: "Lorem it is short description",
-  },
-];
 
 const Appointment = () => {
   const { id } = useParams();
